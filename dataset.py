865093d0cd38ce0c09c3efa4893d6fe7313bac71
lenovo-win <lenovo-win>
2023-03-03 13:52:36 +0800

    update

--- dataset.py
+++ dataset.py
@@ -1,7 +1,6 @@
 import torch
 from PIL import Image
 import os
-import numpy as np
 
 
 def get_data_and_label(root_dir, is_train=True):
@@ -20,16 +19,12 @@ def get_data_and_label(root_dir, is_train=True):
     return images_path, labels_path
 
 
-def preprocess_input(image):
-    image /= 255.0
-    return image
-
-
 class Mydataset(torch.utils.data.Dataset):
     """自定义数据集"""
 
-    def __init__(self, root_dir, is_train=True):
+    def __init__(self, root_dir, is_train=True, transform=None):
         self.images_path, self.images_labels = get_data_and_label(root_dir=root_dir, is_train=is_train)
+        self.transform = transform
 
     def __len__(self):
         return len(self.images_path)
@@ -37,6 +32,10 @@ class Mydataset(torch.utils.data.Dataset):
     def __getitem__(self, item):
         img = Image.open(self.images_path[item])
         label = Image.open(self.images_labels[item])
-        img = np.transpose(preprocess_input(np.array(img, np.float32)), [2, 0, 1])
-        label = np.array(label)
+        # RGB为彩色图片，L为灰度图片
+        if img.mode != 'RGB':
+            raise ValueError("image: {} isn't RGB mode.".format(self.images_path[item]))
+        if self.transform is not None:
+            img = self.transform(img)
+            label = self.transform(label)
         return img, label
